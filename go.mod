0a6954e2fd7725817c66e78d16cf3264b06db896
Jan Semmelink <jansemmelink@conor.co.za>
2019-08-09 04:24:29 +0200

    Added request fields with validation.

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.12
 
 replace github.com/go-msvc/domain => ../domain
 
-require github.com/go-msvc/domain v0.0.0-00010101000000-000000000000
+require (
+	github.com/go-msvc/domain v0.0.0-00010101000000-000000000000
+	github.com/jansemmelink/log v0.3.0
+)
--- hello/main.go
+++ hello/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/go-msvc/domain"
 	"github.com/go-msvc/domain/server/rest"
+	"github.com/jansemmelink/log"
 )
 
 func main() {
+	log.DebugOn()
 	domain.New("hello").
 		WithOper("greet", greet{}).
 		WithOper("sing", sing{}).
@@ -14,14 +16,31 @@ func main() {
 }
 
 //Greet ...
-type greet struct{}
+type greet struct {
+	Name string `json:"name"`
+}
+
+type greetResponse struct {
+	Message string `json:"message"`
+}
+
+func (g greet) Validate() error {
+	if len(g.Name) == 0 {
+		return log.Wrapf(nil, "missing name")
+	}
+	return nil
+}
 
 func (g greet) Run() (domain.IResponse, error) {
-	return "Hi", nil
+	return greetResponse{Message: "Hi " + g.Name + "!"}, nil
 }
 
 //Sing ...
 type sing struct{}
+
+func (s sing) Validate() error {
+	return nil
+}
 
 func (s sing) Run() (domain.IResponse, error) {
 	return "La-la-la", nil
